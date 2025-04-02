97e962c9776191aec6c73bb41b6401bfd47837fb
chasov-artem <chasov90@gmail.com>
2025-04-02 23:02:17 +0300

    labels

--- src/app/components/active-label.tsx
+++ src/app/components/active-label.tsx
@@ -1,1 +1,14 @@
+import React from 'react';
+import s from "./active-label.module.css";
+
+export interface ActiveLabelProps {
+  children: React.ReactNode;
+  
+}
+
+export default function ActiveLabel({children}: ActiveLabelProps) {
+  return (<span className={s.label}>{children}</span>);
+}
+
+
 
