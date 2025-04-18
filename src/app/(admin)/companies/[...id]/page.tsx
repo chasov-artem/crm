60b6c4bac1930e81f632c60d6c57d29c8e4e1adf
chasov-artem <chasov90@gmail.com>
2025-04-18 15:24:00 +0300

    not-found

--- src/app/(admin)/companies/[...id]/page.tsx
+++ src/app/(admin)/companies/[...id]/page.tsx
@@ -1,17 +1,19 @@
-import React from 'react';
-
+import { notFound } from 'next/navigation';
 import Header from '@/app/components/header';
 
 export interface PageProps {
-  params: Promise<{ id: string[] }>;
+  params: { id: string };
 }
 
-export default async function Page({ params }: PageProps) {
-  const resolvedParams = await params;
+export default function Page({ params }: PageProps) {
+  const id = Number.parseInt(params.id);
+  if (Number.isNaN(id)) {
+    notFound();
+  }
+
   return (
     <>
-      <Header>Companies ({String(resolvedParams.id)})</Header>
-      <p>{new Date().toTimeString()}</p>
+      <Header>Companies ({id})</Header>
     </>
   );
 }
